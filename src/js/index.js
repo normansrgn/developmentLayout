d314093cee80efa447427df86fd7c78bfec7fd13
normansrgn <normanmagic2004@gmail.com>
2024-06-23 09:50:15 +0300

    header

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
+
+import burgerMenu from './modules/burgermenu.jsx';
+burgerMenu();
+
+import search from './modules/search.js';
+search(); 
+
+import * as flsFunctions from "./modules/tyoetext.js";
+flsFunctions.isWebp();
+
+// import isAvifWebp from 'avif-webp-checker';
+// isAvifWebp({ mode: 'webp' });
+ 
